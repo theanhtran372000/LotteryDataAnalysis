39a8ea8f50e0bd5ea0bc057d9f9fbc543d888361
theanhtran-pixta <theanh.tran@pixta.co.jp>
2022-07-14 17:14:34 +0700

    chore(*) optimize streamlit display, add style.css, README.md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# LotteryDataAnalysis
+# Phân tích dữ liệu lô đề miền Bắc
+
+Bài tập lớn môn Lưu trữ và xử lý dữ liệu lớn do **TS. Nguyễn Hữu Đức** hướng dẫn.
+
+## 1. Phân công công việc
+- Trần Thế Anh (20183688): Nhóm trưởng, cài đặt Spark trên Yarn của HDFS, phân tích dữ liệu bằng Spark, làm slide
+- Nguyễn Quang Hưng (20183760): Cài đặt Kafka, viết chương trình producer và consumer cho kafka
+- Phạm Xuân Lộc (20183786): Tìm nguồn dữ liệu, tìm cách crawl dữ liệu
+- Trần Đình Nguyên (20183807): Visualize dữ liệu phân tích được bằng Streamlit 
+- Nguyễn Huy Hiếu (20183742): Cài đặt HDFS và kết nối tới Kafka
+- Cả nhóm: Lên kế hoạch, đề tài và viết báo cáo
+
+## 2. Mô tả bài tập lớn
+### a. Dòng dữ liệu
+
+![System](./imgs/system.png)
+
+**Mô tả:** Nhìn chung, dữ liệu sẽ được crawl từ một trang web về kết quả xổ số miền Bắc [ketqua1.net](https://ketqua1.net/so-ket-qua), sau đó dữ liệu được đẩy vào Kafka để trung chuyển. Tiếp theo, Kafka sẽ lưu dữ liệu gửi tới vào HDFS. Kế tiếp, Spark thông qua YARN để đọc dữ liệu từ HDFS và phân tích dữ liệu, dữ liệu được phân tích xong sẽ được lưu vào hệ thống file local. Cuối cùng, một chương trình được build bằng thư viện Streamlit sẽ đọc dữ liệu đã phân tích và visualize lên.
+
+### b. Đặc điểm nổi bật
+- Hadoop và Spark được phân tán trên nhiều máy ảo
+- Hệ thống có thể chạy continuous, tức là có dữ liệu mới thì toàn bộ data flow sẽ được update lại
+
+### c. Hình ảnh minh họa
+Kết quả cuối cùng được visualize trên Streamlit như sau:
+
+![Streamlit](./imgs/example.png)
--- streamlit/app.py
+++ streamlit/app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
-import time
 from datetime import datetime
-import yaml
+
+DISPLAY_MAX = 8
+DISPLAY_DEFAULT = 4
 
 st.set_page_config(layout='wide')
 st.title('Phân tích dữ liệu lô đề')
+
+# Thêm style cho page
+with open('./style.css') as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 # Kết quả 100 ngày gần đây
 df = pd.read_csv('./results/100.csv')
@@ -14,7 +19,7 @@ result = df.values
 # --- Đề 2 số --- #
 st.subheader('Đề 2 số')
 st.markdown('**Kết quả những ngày gần đây**')
-latest_n2 = st.select_slider('Số ngày', range(1, 11), value=3, key='latest_n2')
+latest_n2 = st.select_slider('Số ngày', range(1, DISPLAY_MAX + 1), value=DISPLAY_DEFAULT, key='latest_n2')
 latest_col2 = st.columns(latest_n2)
 df2_latest_list = []
 for i in range(latest_n2):
@@ -27,7 +32,7 @@ df2_graph = st.bar_chart(df2.set_index('number'))
 st.markdown('**Những số đề trúng nhiều nhất**')
 df2_sorted = sorted([(i[1], str(i[0])) for i in df2.values], reverse=True)
 
-top_n2 = st.select_slider('Top đề 2 số', range(1, 11), value=3)
+top_n2 = st.select_slider('Top đề 2 số', range(1, DISPLAY_MAX + 1), value=DISPLAY_DEFAULT)
 col2 = st.columns(top_n2)
 df2_top_list = []
 for i in range(top_n2):
@@ -36,7 +41,7 @@ for i in range(top_n2):
 # --- Đề 3 số --- #
 st.subheader('Đề 3 số')
 st.markdown('**Kết quả những ngày gần đây**')
-latest_n3 = st.select_slider('Số ngày', range(1, 11), value=3, key='latest_n3')
+latest_n3 = st.select_slider('Số ngày', range(1, DISPLAY_MAX + 1), value=DISPLAY_DEFAULT, key='latest_n3')
 latest_col3 = st.columns(latest_n3)
 df3_latest_list = []
 for i in range(latest_n3):
@@ -49,7 +54,7 @@ df3_graph = st.bar_chart(df3.set_index('number'))
 st.markdown('**Những số đề trúng nhiều nhất**')
 df3_sorted = sorted([(i[1], str(i[0])) for i in df3.values], reverse=True)
 
-top_n3 = st.select_slider('Top đề 3 số', range(1, 11), value=3)
+top_n3 = st.select_slider('Top đề 3 số', range(1, DISPLAY_MAX + 1), value=DISPLAY_DEFAULT)
 col3 = st.columns(top_n3)
 df3_top_list = []
 for i in range(top_n3):
@@ -72,70 +77,8 @@ odd_rate = odd_cnt / total * 100
 even_rate = 100 - odd_rate
 
 
-st.markdown('**Số lần xuất hiện chẵn và lẻ**')
+st.markdown('**Xác suất xuất hiện chẵn và lẻ**')
 col = st.columns(3)
 total_col = col[0].metric('Tổng số', '100%', '{} lần'.format(total))
 odd_col = col[1].metric('Số lẻ', '{:.2f}%'.format(odd_rate), '{} lần'.format(odd_cnt))
 even_col = col[2].metric('Số chẵn', '{:.2f}%'.format(even_rate),'{} lần'.format(even_cnt))
-
-# while True:
-#     print('- ' * 20)
-#     print('Check data at {} ...'.format(datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
-#     with open('./config.yaml', 'r') as f:
-#         config = yaml.load(f, yaml.loader.SafeLoader)
-#         n_samples = config['hdfs_count']
-        
-#     # Có dữ liệu mới
-#     if n_samples > total:
-#         print('There are new data. Updating stats ...')
-        
-#         # Update dữ liệu đề 2 số
-#         df2 = pd.read_csv('./results/2digit.csv')
-        
-#         # Update graph
-#         df2_graph.bar_chart(df2.set_index('number'))
-
-#         # Update top
-#         df2_sorted = sorted([(i[1], str(i[0])) for i in df2.values], reverse=True)
-#         for i in range(top_n2):
-#             df2_top_list[i].metric('Top {}'.format(i + 1), 'Số {:02d}'.format(int(df2_sorted[i][1])), '{} lần'.format(df2_sorted[i][0]))
-            
-            
-#         # Update dữ liệu đề 3 số
-#         df3 = pd.read_csv('./results/3digit.csv')
-        
-#         # Update graph
-#         df3_graph.bar_chart(df3.set_index('number'))
-
-#         # Update top
-#         df3_sorted = sorted([(i[1], str(i[0])) for i in df3.values], reverse=True)
-#         for i in range(top_n3):
-#             df3_top_list[i].metric('Top {}'.format(i + 1), 'Số {:03d}'.format(int(df3_sorted[i][1])), '{} lần'.format(df3_sorted[i][0]))
-            
-            
-#         # Update đề chẵn lẻ
-#         # Tính lại thông số
-#         odd_cnt = 0     # Số lần lẻ
-#         even_cnt = 0    # Số lần chẵn
-
-#         for item in df2_sorted:
-#             number = int(item[1])
-#             if number % 2 == 0:
-#                 even_cnt += int(item[0])
-#             else:
-#                 odd_cnt += int(item[0])
-
-#         total = odd_cnt + even_cnt
-#         odd_rate = odd_cnt / total * 100
-#         even_rate = 100 - odd_rate
-        
-#         # Hiển thị lại
-#         total_col.metric('Tổng số', '100%', '{} lần'.format(total))
-#         odd_col.metric('Số lẻ', '{:.2f}%'.format(odd_rate), '{} lần'.format(odd_cnt))
-#         even_col.metric('Số chẵn', '{:.2f}%'.format(even_rate),'{} lần'.format(even_cnt))
-#         print('Finished!')
-#     else:
-#         print('Nothing changed!')
-        
-#     print()
-#     time.sleep(5 * 60) # Check 5p 1 lần
